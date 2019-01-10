1df969346796902526b629eec67709920af8dafc
Marco Esposito <marco.esposito@tum.de>
2019-01-10 01:06:41 +0100

    Changed license to LGPLv3

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ except ImportError:
 
 setup(
     name='trackingmore',
-    version='0.1',
+    version='0.2',
     packages=['trackingmore',],
     author='Marco Esposito',
     author_email='marcoesposito1988@gmail.com',
-    license='GPLv2',
+    license='LGPLv3',
     description='Python wrapper for the TrackingMore API',
     url='https://github.com/marcoesposito1988/trackingmore-python',
     install_requires=['requests >= 0.8.8'],
